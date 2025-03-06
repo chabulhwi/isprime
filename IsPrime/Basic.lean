89b94499e1f3e04b106117d03fa2f7613ebef964
Bulhwi Cha <chabulhwi@semmalgil.com>
2025-03-07 00:35:36 +0900

    fix: make sure `Nat.isPrime 2` is `true`

--- IsPrime/Basic.lean
+++ IsPrime/Basic.lean
@@ -1,28 +1,29 @@
 namespace Nat
 
-def isPrime.loop (divisor n : Nat) (h₁ : divisor ≥ 2) (h₂ : n ≥ 2) : Bool :=
-  if divisor ∣ n then
-    false
-  else if divisor ^ 2 ≤ n then
-    loop (divisor + 1) n (Nat.le_trans h₁ (Nat.le_succ divisor)) h₂
+def isPrime.loop (n divisor : Nat) (h₁ : n ≥ 2) (h₂ : divisor ≥ 2) : Bool :=
+  if divisor ^ 2 ≤ n then
+    if divisor ∣ n then
+      false
+    else
+      loop n (divisor + 1) h₁ (Nat.le_trans h₂ (Nat.le_succ divisor))
   else
     true
 termination_by n - divisor
 decreasing_by
-  next hpw2 =>
+  next hpw2 _ =>
   have hltp : divisor < divisor ^ 2 := by
     conv => left; rw [← Nat.mul_one divisor]
     rw [Nat.pow_two]
-    exact Nat.mul_lt_mul_of_pos_left (lt_of_succ_le h₁) (Nat.lt_of_lt_of_le Nat.two_pos h₁)
+    exact Nat.mul_lt_mul_of_pos_left (lt_of_succ_le h₂) (Nat.lt_of_lt_of_le Nat.two_pos h₂)
   have hltn : divisor < n := Nat.lt_of_lt_of_le hltp hpw2
   simp only [sub_add_eq]
   exact sub_one_lt <| ne_of_gt (Nat.sub_pos_of_lt hltn)
 
-/-- `isPrime` tests whether a natural number is prime. -/
+/-- `Nat.isPrime` tests whether a natural number is prime. -/
 def isPrime (n : Nat) : Bool :=
   if h : n ≤ 1 then
     false
   else
-    isPrime.loop 2 n (Nat.le_refl 2) (gt_of_not_le h)
+    isPrime.loop n 2 (gt_of_not_le h) (Nat.le_refl 2)
 
 end Nat
